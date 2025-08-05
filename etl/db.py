51cb927534164814c8121212a845d726f19a0906
Malik Vance <malikovance@gmail.com>
2025-08-04 20:24:22 -0500

    ajdusted files for security.

--- etl/db.py
+++ etl/db.py
@@ -1,10 +1,15 @@
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
 
 def get_db_connection():
     return psycopg2.connect(
-        dbname="joy_of_painting",
-        user="bob_ross",
-        password="*****",
-        host="localhost",
-        port="5432"
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
     )
